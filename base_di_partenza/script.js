897efa9bcceb91c6ff0343a3979d163da3c22a88
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-04-05 03:20:43 +0200

    Finished

--- base_di_partenza/script.js
+++ base_di_partenza/script.js
@@ -1,13 +1,10 @@
-const headerNav = document.getElementById("nav-section");
-const navBtn = document.getElementById("green-btn");
 window.addEventListener("scroll", () => {
-  let height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
+  const headerNav = document.getElementById("nav-section");
+  const navBtn = document.getElementById("green-btn");
+  const headerHeight = document.querySelector("header").offsetHeight;
+  const headerHeightPercentage = (headerHeight * 70) / 100;
   const scrollPosition = window.scrollY;
-  const scrolled = (scrollPosition / height) * 100;
-  console.log(scrolled);
-  if (scrolled > 22) {
+  if (scrollPosition > headerHeightPercentage) {
     headerNav.classList.add("nav-scroll");
     navBtn.classList.add("nav-btn-scroll");
   } else {
@@ -15,3 +12,13 @@ window.addEventListener("scroll", () => {
     navBtn.classList.remove("nav-btn-scroll");
   }
 });
+
+setInterval(() => {
+  const m = document.getElementsByTagName("g");
+  const randNum = Math.floor(Math.random() * m.length);
+  const randIndex = m[randNum];
+  randIndex.classList.add("letter-opacity");
+  setTimeout(() => {
+    randIndex.classList.remove("letter-opacity");
+  }, 1500);
+}, 50);
